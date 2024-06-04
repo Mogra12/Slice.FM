1f2507442fb61102f5f1fca9fc837ec1f78c7f00
ruanv <86124740+Mogra12@users.noreply.github.com>
2024-06-03 22:24:13 -0300

    Add files via upload

--- pages/site/script_main_site.js
+++ pages/site/script_main_site.js
@@ -1,18 +1,33 @@
+const audioPlayer1 = document.getElementById('audio-player-1')
+const progressBarPlayer1 = document.getElementById('audio-progress-player1')
+const timeupdatePlayer1 = document.getElementById('timerupdate-audio')
+const spantimeupdatePlayer1 = document.getElementById('span-timeupdate')
 
-const audio = window.document.getElementById('audio-player-1')
-const play_button = window.document.getElementById('play-button-player1')
-const pause_button = window.document.getElementById('pause-button-player1')
-const progress_bar = window.document.getElementById('audio-progress-player1')
-
-function play() {
-    audio.play()
+function playAudio() {
+    audioPlayer1.play()
 }
 
-function pause() {
-    audio.pause()
+function pauseAudio() {
+    audioPlayer1.pause()
 }
 
-audio.addEventListener('timeupdate', () => {
-    var progress = (audio.currentTime / audio.duration) * 100
-    progressBar.style.width = progress + '%'
-})
+function updateProgressBar() {
+    const percentage = (audioPlayer1.currentTime / audioPlayer1.duration) * 100
+    progressBarPlayer1.value = percentage
+}
+
+function seekAudio() {
+    const seekTime = (progressBarPlayer1.value / 100) * audioPlayer1.duration
+    audioPlayer1.currentTime = seekTime
+}
+ 
+function timerUpdaterAudio() {
+    const durationAudio = audioPlayer1.duration
+    timeupdatePlayer1.innerText = durationAudio
+}
+
+
+spantimeupdatePlayer1.innerText = `${a}`
+
+audioPlayer1.addEventListener('timeupdate', updateProgressBar)
+progressBarPlayer1.addEventListener('input', seekAudio)
